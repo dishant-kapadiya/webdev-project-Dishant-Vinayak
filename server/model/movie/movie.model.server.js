20a41a81b700da63d6f94195a38d0e5f599ff9bd
Dishant Kapadiya <kapadiya.d@husky.neu.edu>
2017-12-09 20:54:34 -0500

    updated movie.model functions

--- server/model/movie/movie.model.server.js
+++ server/model/movie/movie.model.server.js
@@ -3,34 +3,28 @@ let mongoose = require('mongoose');
 let movieSchema = require('./movie.schema.server');
 let movieModel = mongoose.model('MovieModel', movieSchema);
 
+module.exports = movieModel;
+
 movieModel.createMovie = function (movie) {
 	return movieModel.create(movie);
 };
-movieModel.findAllMovies = findAllMovies;
-movieModel.findMovieById = findMovieById;
-movieModel.addRating = addRating;
-movieModel.addReview = addReview;
-movieModel.findMovieByObjectId = findMovieByObjectId;
-module.exports = movieModel;
 
-
-
-function findMovieById(movieId) {
-	return movieModel.findOne({movieId: movieId});
-}
-
-function findMovieByObjectId(movieId) {
-	return movieModel.findOne({_id: movieId});
-}
-
-function findAllMovies() {
+movieModel.findAllMovies = function () {
 	return movieModel.find();
-}
+};
 
-function addRating(movieId, rating) {
+movieModel.findMovieById = function findMovieById(movieId) {
+	return movieModel.findOne({movieId: movieId});
+};
+
+movieModel.addRating = function (movieId, rating) {
 	return movieModel.update({'movieId': movieId}, {$push:{ratings:rating.rating}});
-}
+};
 
-function addReview(movieId, review) {
+movieModel.addReview = function (movieId, review) {
 	return movieModel.update({'movieId': movieId}, {$push: {reviews: review.review}});
-}
+};
+
+movieModel.findMovieByObjectId = function (movieId) {
+	return movieModel.findOne({_id: movieId});
+};
