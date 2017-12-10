d206cbce60143d21abca08585e78f80160187510
Vinayak Shamnani <shamnani.v@husky.neu.edu>
2017-12-09 21:07:49 -0500

    added highest grossing api

--- server/services/search.service.server.js
+++ server/services/search.service.server.js
@@ -1,41 +1,70 @@
 module.exports = function (app) {
-  app.post('/search/movies', searchMoviesHandler);
+    app.post('/search/movies', searchMoviesHandler);
+    app.get('/search/highestgrossing', searchHighestGrossing);
 
-  function searchMoviesHandler(req, res) {
-    searchMovies(req.body.query)
-      .then((result) => {
-        res.status(200);
-        res.send(result);
-      })
-      .catch((error) => {
-        res.status(400);
-        res.send(error);
-      })
-  }
+    function searchMoviesHandler(req, res) {
+        searchMovies(req.body.query)
+            .then((result) => {
+                res.status(200);
+                res.send(result);
+            })
+            .catch((error) => {
+                res.status(400);
+                res.send(error);
+            })
+    }
 
-  function searchMovies(searchQuery) {
-    const API_KEY = process.env.TMDB_KEY || 'd3d04ee78153a24eeb1f7a0e73f56a9c';
-    const TMDB_URL = 'https://api.themoviedb.org/3/search/movie';
+    function searchMovies(searchQuery) {
+        const API_KEY = process.env.TMDB_KEY || 'd3d04ee78153a24eeb1f7a0e73f56a9c';
+        const TMDB_URL = 'https://api.themoviedb.org/3/search/movie';
 
-    let options = {
-      uri: TMDB_URL,
-      qs: {
-        api_key: API_KEY,
-        query: searchQuery
-      },
-      json: true // Automatically parses the JSON string in the response
-    };
+        let options = {
+            uri: TMDB_URL,
+            qs: {
+                api_key: API_KEY,
+                query: searchQuery
+            },
+            json: true // Automatically parses the JSON string in the response
+        };
+    }
 
-    return new Promise(function (resolve, reject) {
-      require('request-promise')(options)
-        .then((response) => {
-          resolve(response.results)
-        })
-        .catch((err) => {
-          reject({
-            "message": "Bad Request"
-          })
+
+    function searchHighestGrossing(req, res) {
+        searchMoviesByRevenue()
+            .then((result) => {
+                res.status(200);
+                res.send(result);
+            })
+            .catch((error) => {
+                res.status(400);
+                res.send(error);
+            })
+    }
+
+    function searchMoviesByRevenue() {
+        const API_KEY = process.env.TMDB_KEY || 'd3d04ee78153a24eeb1f7a0e73f56a9c';
+        const TMDB_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc';
+
+        let options = {
+            uri: TMDB_URL,
+            qs: {
+                api_key: API_KEY
+            },
+            json: true // Automatically parses the JSON string in the response
+        };
+
+
+        return new Promise(function (resolve, reject) {
+            require('request-promise')(options)
+                .then((response) => {
+                    resolve(response.results)
+                })
+                .catch((err) => {
+                    reject({
+                        "message": "Bad Request"
+                    })
+                });
         });
-    });
-  }
+    }
+
 };
