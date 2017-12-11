036b1263fd49a0c5880ccb5c8b8d0f5fafdac62d
Dishant Kapadiya <kapadiya.d@husky.neu.edu>
2017-12-11 01:38:19 -0500

    modified Review model

--- server/model/review/review.schema.server.js
+++ server/model/review/review.schema.server.js
@@ -2,7 +2,7 @@ let mongoose = require('mongoose');
 module.exports = mongoose.Schema({
 	ownerId: {type: mongoose.Schema.ObjectId, ref: 'user'},
 	username: String,
-	movieId: {type: mongoose.Schema.String, ref: 'movie.movieId'},
+	movieId: {type: mongoose.Schema.ObjectId, ref: 'movie'},
 	review: String,
 	flagged: String,
 	dateCreated: {type: Date, default: Date.now()}
